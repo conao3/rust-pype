08151b58f4ff92b5a94a7065915ac6576d0afe00
Naoya Yamashita <conao3@gmail.com>
2023-02-24 23:49:30 +0900

    implement first pype

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs, io, process};
+use std::{
+    fs,
+    io::{self, Write},
+    process,
+};
 
 fn main() {
     let tmp_dir = tempfile::tempdir().unwrap();
@@ -6,11 +10,19 @@ fn main() {
     let fifo_path = tmp_dir.path().join(format!("pype__{pid}.fifo"));
     nix::unistd::mkfifo(&fifo_path, nix::sys::stat::Mode::S_IRWXU).unwrap();
 
+    let fifo_path_str = fifo_path.to_str().unwrap();
+    print!(
+        r###"
+with open("{fifo_path_str}") as f:
+    for line in f:
+        print(line, end="")
+"###
+    );
+    io::stdout().flush().unwrap();
+    nix::unistd::close(1).unwrap();
+
     let r = io::stdin();
     let mut reader = r.lock();
-
-    println!("Writing to {}", fifo_path.display());
-
     let mut w = fs::File::create(&fifo_path).unwrap();
 
     // TODO: receive C-c, cleanup tempdir
