680d30a35e32ea4f964a3fdc7ec59a70d240ce11
Naoya Yamashita <conao3@gmail.com>
2023-03-04 00:54:50 +0900

    commentout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused_imports)]
+
+use pype::types;
+
 use std::{
     fs,
     io::{self, Write},
@@ -18,6 +22,66 @@ with open("{fifo_path_str}") as f:
         print(line, end="")
 "###
     );
+    // let print_expr = types::expression::PyCall {
+    //     function: Box::new(types::expression::PyIdent::from("print").into()),
+    //     arguments: vec![
+    //         types::expression::PyIdent::from("line").into(),
+    //         ("end", "").into(),
+    //     ],
+    // };
+    // let for_expr = types::statement::PyFor {
+    //     targets: vec![types::expression::PyIdent::from("line")],
+    //     iterator: Box::new(types::expression::PyIdent::from("f").into()),
+    //     body: vec![print_expr],
+    // };
+    // let program = types::statement::PyProgram {
+    //     statements: vec![
+    //         types::statement::PyWith {
+    //             expression: types::expression::PyCall {
+    //                 function: types::expression::PyIdent {
+    //                     ident: "open".to_string(),
+    //                 },
+    //                 arguments: vec![types::expression::PyString {
+    //                     string: fifo_path_str.to_string(),
+    //                 }],
+    //             },
+    //             target: types::expression::PyIdent {
+    //                 ident: "f".to_string(),
+    //             },
+    //             body: vec![
+    //                 types::statement::PyFor {
+    //                     target: types::expression::PyIdent {
+    //                         ident: "line".to_string(),
+    //                     },
+    //                     iterator: types::expression::PyCall {
+    //                         function: types::expression::PyIdent {
+    //                             ident: "f".to_string(),
+    //                         },
+    //                         arguments: vec![],
+    //                     },
+    //                     body: vec![types::statement::PyPrint {
+    //                         expression: types::expression::PyCall {
+    //                             function: types::expression::PyIdent {
+    //                                 ident: "print".to_string(),
+    //                             },
+    //                             arguments: vec![
+    //                                 types::expression::PyIdent {
+    //                                     ident: "line".to_string(),
+    //                                 },
+    //                                 types::PyKeywordArgument {
+    //                                     name: "end".to_string(),
+    //                                     expression: types::expression::PyString {
+    //                                         string: "".to_string(),
+    //                                     },
+    //                                 },
+    //                             ],
+    //                         },
+    //                     }],
+    //                 },
+    //             ]
+    //         }
+    //     ]
+    // };
     io::stdout().flush().unwrap();
     nix::unistd::close(1).unwrap();
 
